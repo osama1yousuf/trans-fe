30ed9fa4cf1bc527c490630f604facf122d9f07b
Muhammad Osama <osama@strugbitsglobal.com>
2025-01-02 18:11:56 +0500

    date format

--- src/app/admin/driver/page.js
+++ src/app/admin/driver/page.js
@@ -551,7 +551,7 @@ export default function ActiveDriver() {
                                   Joining Date:
                                 </span>
                                 <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80">
-                                  {moment(row.joiningDate).format(
+                                  {moment(item?.joiningDate).format(
                                     "DD-MMM-YYYY"
                                   )}
                                 </div>
