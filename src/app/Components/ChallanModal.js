078253325829d449d029687052b644c267bd8542
Adil Arif <arifadil739@gmail.com>
2025-06-06 00:55:38 +0500

    [fix] modal-width

--- src/app/Components/ChallanModal.js
+++ src/app/Components/ChallanModal.js
@@ -105,7 +105,7 @@ const ChallanModal = ({ setChallanModal, handlePayNow, type, customer }) => {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-6xl">
+        <div className="relative w-auto my-6 mx-auto md:max-w-6xl max-sm:max-w-[93vw]">
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
