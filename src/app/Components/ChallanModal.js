1a54716b987678a19b20006a3f9acadf6ee40574
Adil Arif <arifadil739@gmail.com>
2025-07-07 01:37:11 +0500

    fix

--- src/app/Components/ChallanModal.js
+++ src/app/Components/ChallanModal.js
@@ -185,7 +185,7 @@ const ChallanModal = ({
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto md:max-w-6xl max-sm:max-w-[95vw]">
+        <div className="relative w-auto my-6 mx-auto md:max-w-6xl max-sm:max-w-[95vw] max-h-[90vh]">
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
